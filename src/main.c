229bcb0e8c13536a10b7c2ae4798f700710d225a
Raihan-28011 <raihanrifar4@gmail.com>
2020-12-10 03:20:36 +0600

    Refactored main

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include <inttypes.h>
@@ -6,65 +7,111 @@
 #include <string.h>
 #include <stdint.h>
 
-enum commands {
-    LIST, CREATE,
-    INSERT, FINISH, 
-    LOAD, ADD,
-    GET, CLOSE,
-    SHOWALL, 
-    HELP, EXIT
+
+/* Declaring command enums */
+enum commands {             /*  Values */
+    LIST    ,   CREATE  ,   /* 0 , 1 , */
+    INSERT  ,   FINISH  ,   /* 2 , 3 , */ 
+    LOAD    ,   ADD     ,   /* 4 , 5 , */
+    GET     ,   CLOSE   ,   /* 6 , 7 , */
+    SHOW    ,   HELP    ,   /* 8 , 9 , */
+    EXIT                    /* 10      */
 };
 
-char const *Commands[] = { "list", "create", "insert", "finish", 
-                    "load", "add", "get", "close", "show-all", "help", "exit"
-                  };
+/* These are the commands that will be used in the system as commands and subcommands */
+char const *Commands[] = { 
+    "list"  ,   "create",   /* Commands[LIST]   , Commands[CREATE], */
+    "insert",   "finish",   /* Commands[INSERT] , Commands[FINISH], */
+    "load"  ,   "add"   ,   /* Commands[LOAD]   , Commands[ADD]   , */
+    "get"   ,   "close" ,   /* Commands[GET]    , Commands[CLOSE] , */
+    "show"  ,   "help"  ,   /* Commands[SHOW]   , Commands[HELP]  , */
+    "exit"                  /* Commands[EXIT]   ,                   */
+};
 
+
+/* These are the fields of the database */
 typedef struct Fields {
     char name[34];
     char phone[12];
     char email[34];
 } Fields;
 
+/* This is the Table which contains the data */
 typedef struct table {
-    uint32_t no_of_records;
+    uint32_t no_of_records; /* This will store the number of total records that has been entered */
     Fields records[20];
 } Table;
 
-void help_ndbs();
-void command_error(char const *command, void (*help) (void));
 
-void help_list();
+/* Function type */
+typedef void Func_t (void);
+
+/* Typedef to declare index type for arrays */
+typedef ptrdiff_t Index_t;
+
+/* Typedef for counter type */
+typedef uint8_t Counter;
+
+/* To avoid null statement */
+#define IGNORE
+
+
+/*           Function declarations 
+ + -----------------         ----------------+ */
+
+/* Functions for 'list' commands */
 void list_database();
 
-void help_create();
-void insert_record();
-void create_database();
 
-void help_load();
+/* Functions for 'create' command */
+void create_database();
+void insert_record();
+
+
+/* Functons for 'load' command */
 void add_record();
 void get_record();
 void show_all_records();
 void close_database();
 void load_database();
 
-void print_command_prompt(char const *command);
+
+/* Fnctions for handling command errors and to print out help */
+void help_ndbs(int argc, char const *argv);
+void help_ndbs_commands();
+void help_list();
+void help_create();
+void help_load();
+
+
+/* This function takes the unrecognized command name and the appropriate help function pointer */ 
+void command_error(char const *command, Func_t *func);
+
+
 void table_write(char const *fname, Table *table);
 
-bool equal(char const *str1, char const *str2)
-{
-    return strcmp(str1, str2) == 0;
-}
 
+/*Utility functions */
+void print_ndbs_prompt(); /* prints the main prompt */
+void print_command_prompt(char const *command); /* Prints the prompt till user enters newline */
+void get_command(char *command, uint8_t length); /* gets the command from user and stores it in 'command' */
+bool equal(char const *str1, char const *str2);  /* compares two strings */
+
+
+/* Function that executes operations according to the commands provided by user */
 void load_prompt()
 {
-    printf("ndbs> ");
+    print_ndbs_prompt();
     print_command_prompt("ndbs");
-    char command[7];
-    scanf("%s", command);
 
+    /* As no command is greater than length of 6, we set the size to 7 */
+    char command[7];
+    get_command(command, 7);
+
+    /* while user doesn't want to exit or enters unrecognized command */
     while (true) {
         if (equal(command, Commands[LIST])) {
-            getchar();
+            getchar();  /* eats the newline, enterd by the user after writing a command */
             list_database();
         } else if (equal(command, Commands[CREATE])) {
             create_database();
@@ -73,39 +120,28 @@ void load_prompt()
             load_database();
         } else if (equal(command, Commands[HELP])) {
             getchar();
-            help_ndbs();
+            help_ndbs_commands();
         } else if (equal(command, Commands[EXIT])){
             return;
         } else {
-            command_error(command, help_ndbs);
+            command_error(command, help_ndbs_commands);
             return;
         }
 
-        printf("ndbs> ");
+        print_ndbs_prompt();
         print_command_prompt("ndbs");
-        scanf("%s", command);
-        /* getchar(); */
+        get_command(command, 7);
     }
 }
 
-int main(int argc, char *argv[static 1]) {
-    if (argc >= 2) {
-        if (argc > 2) {
-            fprintf(stderr, "Unknown argumets will be ignored\n");
-        }
+/*          Main Function
+ +---------------      --------------+ */
 
-        if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")) {
-        printf("Usage: ndbs [OPTION]\n"
-                "Options:\n"
-                "  -h --help        Shows this message\n"
-              );
-        } else {
-            fprintf(stderr, "Unknown option %s\n", argv[1]);
-            printf("Usage: ndbs [OPTION]\n"
-                    "Options:\n"
-                    "  -h --help        Shows this message\n"
-                  );
-        }
+int main(int argc, char *argv[static 1]) {
+    static_assert(sizeof(Fields) == 80, "Nope");
+    /* If user wants to know about the application itself */
+    if (argc >= 2) {
+        help_ndbs(argc, argv[1]);
         return 0;
     }
 
@@ -114,7 +150,56 @@ int main(int argc, char *argv[static 1]) {
 }
 
 
-void help_ndbs()
+/*          Functions definitions 
+ +----------------         ---------------+ */
+
+void print_ndbs_prompt()
+{
+    printf("ndbs> ");
+}
+
+void print_command_prompt(char const *command)
+{
+    char c;
+    while ((c = getchar()) == '\n')
+        printf("%s> ", command);
+    ungetc(c, stdin);
+}
+
+void get_command(char *command, uint8_t length)
+{
+    char c;
+    uint8_t i = 0;
+    while ((c = getchar()) != '\n' && --length)
+        command[i++] = c;
+    command[i] = '\0';
+}
+
+bool equal(char const *str1, char const *str2)
+{
+    return strcmp(str1, str2) == 0;
+}
+
+void help_ndbs(int argc, char const *argv_1)
+{
+    char const help_msg[] = { "Usage: ndbs [OPTION]\n"
+                              "Options:\n"
+                              "  -h --help        Shows this message\n"
+                            };
+
+    if (argc > 2)    /* If the user enters unrecognized options */
+        fprintf(stderr, "Warning: Unknown argumets will be ignored\n");
+
+    if (equal(argv_1, "--help") || equal(argv_1, "-h")) {
+        printf("%s", help_msg);
+    } else {
+        fprintf(stderr, "Wrong option '%s'\n", argv_1);
+        printf("%s", help_msg);
+    }
+
+}
+
+void help_ndbs_commands()
 {
     printf("Usage: COMMAND [OPTION]\n"
             "Commands:\n"
@@ -131,65 +216,68 @@ void list_database()
     return;
 }
 
-void print_command_prompt(char const *command)
-{
-    char c;
-    while ((c = getchar()) == '\n')
-        printf("%s> ", command);
-    ungetc(c, stdin);
-}
 
 void help_insert() {
     printf("Under development\n");
 }
 
-void insert_line(char *name, char *phone, char *email)
+void insert_get_line(char *name, char *phone, char *email)
 {
-    char c;
-    int comma = 0;
-    ptrdiff_t namei = 0, phonei = 0, emaili = 0;
-
-    /* name[namei] = '\0'; */
-    /* phone[phonei] = '\0'; */
-    /* email[emaili] = '\0'; */
+    /* We expect input in comma separeted format.
+     * Like this "name, phone_no, email"        */
     
+    char c;
+    Counter comma = 0;
+    Index_t name_i = 0, phone_i = 0, email_i = 0;
+
     while ((c = getchar())) {
-        /* printf("%c %s %s %s\n", c, name, phone, email); */
-        if (c == '$')
-            break;
         if (c == ',') {
             ++comma;
-            if ((c = getchar()) == ' ')
-                ;
+            if ((c = getchar()) == ' ') /* Checks if the input is of "---, --" or "---,--" format. '-' is any char */
+                IGNORE;
             else
                 ungetc(c, stdin);
             continue;
         }
 
-        if (comma < 2 && c == '\n') {
+        if (c == ' ' && (c = getchar()) == ',') /* checks if format is "--- ," or not */
+            continue;
+
+        if (comma < 2 && c == '\n') {  /* if comma count is not equals to 2 but user has entered newline */
             ungetc(c, stdin);
             fprintf(stderr, "Wrong format inserted\n");
             help_insert();
             return;
-        } else if (comma == 2 && c == '\n') {
+        } else if (comma == 2 && c == '\n') {  /* if count is 2 and we encounter newline that means end of input */
             ungetc(c, stdin);
-            printf("Warning: Format inserted without an ending character '$'\n");
             return;
         }
 
-        if (!comma) {
-            name[namei++] = c;
-            name[namei] = '\0';
-        } else if (comma == 1) {
-            phone[phonei++] = c;
-            phone[phonei] = '\0';
-        } else {
-            email[emaili++] = c;
-            email[emaili] = '\0';
+        if (!comma && name_i >= 0) {  /* when comma count is 0, name is expected*/
+            name[name_i++] = c;
+            name[name_i] = '\0';
+            if (name_i + 1 >= 34) {
+                fprintf(stderr, "Warning: length of name has to be between 1-33 characters\n");
+                name_i = -1;
+            }
+        } else if (comma == 1 && phone_i >= 0) {  /* count=1, phone_no is expected */
+            phone[phone_i++] = c;
+            phone[phone_i] = '\0';
+            if (phone_i + 1 >= 34) {
+                fprintf(stderr, "Warning: length of phone number has to be between 1-11 digits\n");
+                phone_i = -1;
+            }
+        } else if (comma == 2 && email_i >= 0) {  /* count=2, email is expected */
+            email[email_i++] = c;
+            email[email_i] = '\0';
+            if (email_i+1 >= 34) {
+                fprintf(stderr, "Warning: length of email has to be between 1-33 characters\n");
+                email_i = -1;
+            }
         }
     }
     
-    if (comma < 2) {
+    if (comma < 2) { /* Less than 2 comma means invalid format */
         fprintf(stderr, "Expected comma seperated format\n");
         help_insert();
     }
@@ -202,7 +290,7 @@ void insert_record(Table *table)
     char name[34] = { "\0" };
     char phone[12] = { "\0" };
     char email[34] = { "\0" };
-    insert_line(name, phone, email);
+    insert_get_line(name, phone, email);
 
     strcpy(table->records[table->no_of_records].name, name); 
     strcpy(table->records[table->no_of_records].phone, phone); 
